2006ede4fd0fbe6b0d2cebd6b7cf7b2883596662
Kirsten <kirsten@kirstens-mbp.dyn.brandeis.edu>
2023-04-27 07:31:35 -0400

    api key added

--- gpt.py
+++ gpt.py
@@ -4,7 +4,7 @@ Code for interacting with GPT-3 in Python.
 import openai
 
 '''Initialize your API key registered from https://platform.openai.com/account/api-keys'''
-key = "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"
+key = "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"
 
 class GPT():
     ''' initialize GPT with apikey '''
