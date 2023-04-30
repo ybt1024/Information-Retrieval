157aa322175ca391a3b04bead6bc045c95e0d3b6
“Tu” <1176255435@qq.com>
2023-04-30 13:15:03 -0400

    adapt code from hw5, add a load function in util

---
+++ es_service/index.py
@@ -0,0 +1,65 @@
+from typing import Iterator, Dict, Union, Sequence, Generator
+
+from elasticsearch_dsl import Index  # type: ignore
+
+from elasticsearch_dsl.connections import connections  # type: ignore
+from elasticsearch.helpers import bulk
+
+from es_service.doc_template import BaseDoc
+
+
+class ESIndex(object):
+    def __init__(
+        self,
+        index_name: str,
+        docs: Union[Iterator[Dict], Sequence[Dict]],
+    ):
+        """
+        ES index structure
+        :param index_name: the name of your index
+        :param docs: wapo docs to be loaded
+        """
+        # set an elasticsearch connection to your localhost
+        connections.create_connection(hosts=["localhost"], timeout=100, alias="default")
+        self.index = index_name
+        es_index = Index(self.index)  # initialize the index
+
+        # delete existing index that has the same name
+        if es_index.exists():
+            es_index.delete()
+
+        es_index.document(BaseDoc)  # link document mapping to the index
+        es_index.create()  # create the index
+        if docs is not None:
+            self.load(docs)
+
+    @staticmethod
+    def _populate_doc(
+        docs: Union[Iterator[Dict], Sequence[Dict]]
+    ) -> Generator[BaseDoc, None, None]:
+        """
+        populate the BaseDoc
+        :param docs: wapo docs
+        :return:
+        """
+        for i, doc in enumerate(docs):
+            es_doc = BaseDoc(_id=i)
+            es_doc.doc_id = doc["_id"]
+            es_doc.title = doc["title"]
+            es_doc.content = doc["text"]
+            es_doc.stemmed_content = doc["text"]
+            es_doc.annotation = doc["annotation"]
+            es_doc.sbert_embedding = doc["sbert_embedding"]
+            yield es_doc
+
+    def load(self, docs: Union[Iterator[Dict], Sequence[Dict]]):
+        # bulk insertion
+        bulk(
+            connections.get_connection(),
+            (
+                d.to_dict(
+                    include_meta=True, skip_empty=False
+                )  # serialize the BaseDoc instance (include meta information and not skip empty documents)
+                for d in self._populate_doc(docs)
+            ),
+        )
