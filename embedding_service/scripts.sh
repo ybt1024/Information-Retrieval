7e82f0a616f42b634401f54421551ff72d21f7b9
“Tu” <1176255435@qq.com>
2023-05-02 21:09:39 -0400

    fix file name

--- embedding_service/scripts.sh
+++ embedding_service/scripts.sh
@@ -3,4 +3,4 @@
 python -m embedding_service.server --embedding sbert  --model all-mpnet-base-v2
 
 # load nf docs into the index called "nf_docs"
-python load_es_index.py --index_name job_posting --corpus_folder_path ./corpus_data/data_job_posts
+python load_es_index.py --index_name job_posting --corpus_folder_path ./corpus_data/data_job_posts.csv
